96be4e681db61056ac6e44ab9e39a5d71bc55adc
SamuelOrtizT <saortizt@unal.edu.co>
2023-08-20 13:35:32 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,3 +6,30 @@ class Asiento:
     def cambiarColor(self, color):
         if color in ["rojo", "verde", "amarillo", "negro", "blanco,"]:
             self.color = color
+class Motor:
+    def __init__ (self, numeroCilindros, tipo, registro):
+        self.numeroCilindros = numeroCilindros
+        self.tipo = tipo
+        self.registro = registro
+    def cambiarRegistro(self, registro):
+        self.registro = registro
+    def asignarTipo(self, tipo):
+        if tipo in ["electrico", "gasolina"]:
+            self.tipo = tipo
+class Auto:
+    cantidadCreados = 0
+    def __init__ (self, modelo, precio, asientos, marca, motor, registro):
+        self.modelo = modelo
+        self.precio = precio
+        self.asientos = asientos
+        self.marca = marca
+        self.motor = motor
+        self.registro = registro
+        Auto.cantidadCreados += 1
+    def cantidadAsientos(self):
+        x = 0
+        for asiento in self.asientos:
+            if type(asiento) == Asiento:
+                x += 1
+        return x
+    
